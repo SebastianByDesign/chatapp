5f1ba3bddec558fab9e720b918252693dc10e6e9
Walter Sebastian Carrera <cwalter1994@gmail.com>
2020-01-28 16:28:37 -0500

    listen for connect/disconnect

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,16 @@
 // imports always go first - if we're importing anything
+
+const socket = io();
+
+function setUserId(packet) {
+    console.log(packet);
+}
+
+function runDisconnectMessage(packet) {
+    console.log(packet);
+}
+
+// event handling
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('user_disconnect', runDisconnectMessage);
